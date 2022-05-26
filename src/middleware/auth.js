d574645873be04050364b25e97d3e03b250006a2
amit token <amitbabhanchak@gmail.com>
2022-05-26 15:39:38 +0530

    same changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,34 +1,55 @@
-const jwt = require('jsonwebtoken')
-const auth=require('../middleware/auth')
-const authentication = async function(req, res, next){
-    try{
-        let token = req.header('Authorization','Bearer');
-      
-        if (!token)
-          return res.status(403).send({ status: false, msg: "Token is required" });
+const jwt = require('jsonwebtoken');
+const Validator = require("../Validator/valid");
 
-        let token1 = token.split(" ").pop()
 
-          
-    let decodedToken = jwt.verify(token1, "project5Group49", {
-        ignoreExpiration: true,
-      }); 
-  
-      if (!decodedToken) {
-        return res
-          .status(403)
-          .send({ status: false, message: "Invalid authentication" });
-      }
-      let exptoken = decodedToken.exp;
-      if (exptoken * 1000 < Date.now())
-        return res.status(400).send({ status: false, msg: "token exp" });
-      req.userId = decodedToken.userId; //error 400
-  
-      next();
+const Authentication = async function (req, res, next) {
+    try {
+        // getting token from req(header)
+        let token = req.headers["x-api-key"];
+        if (!token) token = req.headers["X-Api-Key"];
+        if (!token) {return res.status(400).send({status:false, msg: "Enter x-api-key In Header" });}
+        // token verification
+        // if(req.tokenCheck = token)return res.status(401).send({status : false , message : "Invalid token"})
+       
+        jwt.verify(token,"project5Group49",{ ignoreExpiration: true },function (err, decoded) {
+              if (err) {return res.status(400).send({status : false, meessage : "token invalid"})}
+               else {
+                //The static Date.now() method returns the number of milliseconds elapsed since January 1, 1970
+                if (Date.now() > decoded.exp * 1000) {
+                  return res.status(401).send({status: false,msg: "Session Expired",});
+                }
+                req.userId = decoded.userId;
+                next();
+              }});
+       
     }
-    catch(err){
-        return res.status(500).send({err: err.message})
+    catch (err) {
+        res.status(500).send({ msg: err.message });
     }
 }
+module.exports.Authentication = Authentication;
 
-module.exports.authentication = authentication;
+
+const Authorization = async function (req, res, next) {
+    try {
+        let token = req.headers["x-api-key"];
+        if (!token) token = req.headers["X-Api-Key"]; //taking the x-api-key of value token in headers
+        // check the token are prenent or not in headers
+        if (!token) {return res.status(400).send({status:false, msg: "Enter x-api-key In Header" });}
+        // verify the token 
+        let decodedToken = jwt.verify(token, "project5Group49" )
+         let decoded = decodedToken.userId
+            let userId = req.body.userId;
+            // check the user id present in body
+            if(!Validator.isValid(userId)) return res.status(400).send({status: false,message: "userId is Required"});
+            if(!Validator.isValidObjectId(userId))  return res.status(400).send({status: false,message: "userId is not valid"});
+            //check the  user id are present in decoded token
+            if (userId != decoded) { return res.status(401).send({status:false,msg:"Not Authorised!!"})}
+      
+        next()
+    }
+    catch (err) {
+        return res.status(500).send({ status:false,msg: err.message });
+    }
+}
+module.exports.Authorization = Authorization;
