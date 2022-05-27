dcb7d25f0859755a5202904a010acb785fd1c6a2
thoriumjaspreet <jaspreet98cheemaa@gmail.com>
2022-05-27 17:35:52 +0530

    working

--- src/Controllers/productController.js
+++ src/Controllers/productController.js
@@ -6,7 +6,7 @@ const {uploadFile} = require('../aws/AWS')
 
 const createProduct = async function(req,res){
     try{
-        let data = req.body;
+        let data = JSON.parse(JSON.stringify(req.body))
         let files = req.files;
 
       let {title,description,price,currencyId,currencyFormat,style,availableSizes,installments}=data
@@ -15,11 +15,15 @@ const createProduct = async function(req,res){
       if (!Validator.isValid(title)) return res.status(400).send({ status: false, message: "title is Required" });
       if (!Validator.isValidString(title)) return res.status(400).send({ status: false, message: "title  must be alphabetic characters" })
       
+let isTitlePresent=await productModel.findOne({title})
+if(isTitlePresent) return res.status(400).send({status:false,message:"title is already present"})
+
+
       if (!Validator.isValid(description)) return res.status(400).send({ status: false, message: "description is Required" });
       if (!Validator.isValidString(description)) return res.status(400).send({ status: false, message: "description  must be alphabetic characters" })
       
       if (!(price)) return res.status(400).send({ status: false, message: "price is Required" });
-        if (!!/^[0-9.]{100}$/.test(price)) return res.status(400).send({ status: false, message: "price must be in numeric" })
+      if(!/^[0-9 .]+$/.test(price))  return res.status(400).send({ status: false, message: "price must be in numeric" })
         
 
         if(!(currencyId)) return res.status(400).send({ status: false, message: "currrencyId is Required" });
@@ -57,11 +61,8 @@ const getProductById = async function (req, res) {
 
       if (!Validator.isValidObjectId(productId)) return res.status(400).send({ status: false, message: "Invalid productId" })
 
-      const productData = await productModel
-          .findOne({ _id: productId ,isDeleted:false})
-          .select({ title: 1, _id: 1, description: 1, price: 1, currencyId: 1, currencyFormat: 1,
-            isFreeShipping:1, productImage: 1, style: 1,availableSizes:1,installments:1,isDeleted:1 })
-
+      const productData = await productModel.findOne({_id:productId,isDeleted:false})
+          
       if (!productData) return res.status(404).send({ status: false, message: "product is not found or product is deleted" })
       return res.status(200).send({ status: true, message: "success", data: productData })
   } catch (err) {
@@ -76,8 +77,10 @@ const getProduct = async function (req, res) {
     let query = { isDeleted: false };
     if (filter) {
       const { name,description,isFreeShipping,priceGreaterThan,priceLessThan,style,size,installments,} = filter;
+     
+     
       if (name) {
-        query.title = name.trim();
+        query.title = name.trim()
       }
       if (description) {
         query.description = description.trim();
@@ -112,4 +115,88 @@ const getProduct = async function (req, res) {
 };
 
 
-module.exports={createProduct,getProduct,getProductById}
+
+const deleteProduct = async function (req, res) {
+  let productId = req.params.productId;
+    
+  
+  if (!Validator.isValidObjectId(productId)) return res.status(400).send({ status: false, message: "Invalid productId" })
+ 
+let alreadyDeleted= await productModel.findById(productId)
+if(!alreadyDeleted) return res.status(404).send({status:false,msg:"Data not found"})
+if(alreadyDeleted.isDeleted==false){
+
+  let deletePro=await productModel.findOneAndUpdate(
+    { _id: productId, isDeleted: false },
+    { isDeleted: true, deletedAt:  new String(Date())},{new:true}
+  );
+  
+  return res.status(200).send({ status: true, message: "product deleted successfully", data:deletePro});
+  }else{
+    return res.status(400).send({ status: false, message: "product is already deleted" });
+  }
+}; 
+
+
+
+
+const updateProduct = async function (req, res) {
+  try {
+    let productId = req.params.productId;
+    let data = req.body;
+
+    let {title,description,price,currencyId,currencyFormat,style,availableSizes,installments}=data
+      if (!Validator.isValidReqBody(data)) { return res.status(400).send({ status: false, msg: "Please enter data for update" }) }
+
+ if(title)
+
+ 
+      if (!Validator.isValidString(title)) return res.status(400).send({ status: false, message: "title  must be alphabetic characters" })
+      let isTitlePresent=await productModel.findOne({title})
+      if(isTitlePresent) return res.status(400).send({status:false,message:"title is already present"})
+      
+      if(description)
+
+      if (!Validator.isValidString(description)) return res.status(400).send({ status: false, message: "description  must be alphabetic characters" })
+      
+if(price)
+
+if(!/^[0-9 .]+$/.test(price))  return res.status(400).send({ status: false, message: "price must be in numeric" })
+        
+if(currencyId)
+if(( ["INR"].indexOf(currencyId) == -1) ) return res.status(400).send({status:false,message:"currency Id must be INR"})
+
+if(currencyFormat)
+if(( ["₹"].indexOf(currencyFormat) == -1) ) return res.status(400).send({status:false,message:"currency formet must be ₹ "})
+
+if(style)
+if(!Validator.isValidString(style))return res.status(400).send({ status: false, message: "style must be alphabetic characters" })
+if(availableSizes)
+        if (!Validator.isValidavailableSizes(availableSizes)) return res.status(400).send({ status: false, message: "availabe sizes must be (S, XS,M,X, L,XXL, XL)" })
+
+      if(installments)
+      if(!/^[0-9 ]+$/.test(installments))  return res.status(400).send({ status: false, message: "installments must be in numeric" })
+     
+    let files = req.files;
+    if (files && files.length > 0) {
+      let fileUrl = await uploadFile(files[0]);
+      data.productImage = fileUrl;
+    }
+
+    let updatedData = await productModel.findOneAndUpdate(
+      { _id: productId },
+      data,
+      {
+        new: true,
+      }
+    );
+    return res.status(200).send({
+      status: true,
+      message: "product details updated",
+      data: updatedData,
+    });
+  } catch (err) {
+    return res.status(500).send({ status: false, error: err.message });
+  }
+};
+module.exports={createProduct,getProduct,getProductById,deleteProduct,updateProduct}
