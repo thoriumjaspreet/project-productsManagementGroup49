09bbbb43ac135db8d7da7cabb5fda9ba8f059e3c
amit token <amitbabhanchak@gmail.com>
2022-05-28 10:45:43 +0530

    update is working

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,7 +8,7 @@ const {Authentication,Authorization} =require('../middleware/auth')
 router.post("/register",userController.userCreate);
 router.post("/login",userController.Login);
 router.get("/user/:userId/profile",Authentication,Authorization,userController.getUserById)
-router.put("/user/:userId/profile",Authentication,userController.updatedUser)
+router.put("/user/:userId/profile",Authentication,Authorization,userController.updatedUser)
 //Product APIs
 router.post("/products",productController.createProduct)
 router.get("/products/:productId",productController.getProductById)
