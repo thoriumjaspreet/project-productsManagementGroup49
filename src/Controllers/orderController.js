7fccd6f23d651873414a4ac571c4bbcfd0c0e63d
thoriumjaspreet <jaspreet98cheemaa@gmail.com>
2022-06-01 17:59:06 +0530

    working

--- src/Controllers/orderController.js
+++ src/Controllers/orderController.js
@@ -10,15 +10,40 @@ const Validator = require('../Validator/valid')
     try {
         let user_id = req.params.userId
         let data = req.body
-        const {items,status,cancellable,isDeleted} =data
-        const [{productId,quantity}]= items
+
         let user =await userModel.findById(user_id)
         if(!user) return res.status(404).send({status:false,msg:"User not found"})
+       
+        if(!Validator.isValidReqBody(data)) return res.status(400).send({status:false,msg:"please enter order data"})    
+        const {items,status,cancellable,isDeleted} =data
+        if (toString.call(items) !== "[object Array]"){
+            return res.status(400).send({status:false,msg:"items should be array objects"})
+        }
+
+        const [{productId,quantity}]= items
+
+  if (!Validator.isValidObjectId(productId)) {
+            return res.status(400).send({ status: false, message: "Invalid productId in body" })
+        }
+  
+
+        let product = await productModel.findOne({ _id: productId, isDeleted: false })
+        if (!product) {
+            return res.status(400).send({ status: false, message: "productId does not exits" })
+        }
       
+        if(!quantity) return res.status(400).send({ status: false, msg: "quantity is required" })
+
+    if (!(!isNaN(Number(quantity)))) {
+    return res.status(400).send({ status: false, message: `Quantity should be a valid number` })
+    }
+    if (quantity <= 0) {
+    return res.status(400).send({ status: false, message: `Quantity must be an integer !! ` })
+    }
         // let Cart = await cartModel.findById(cartId)
         // if(!Cart) return res.status(404).send({status:false,msg:"card not found"})
 
-        let cartData = await cartModel.findOne({ _id: cartId, userId: user_id }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+        let cartData = await cartModel.findOne({ userId: user_id }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
         if (!cartData) {
             return res.status(400).send({ status: false, message: "NO cart exist for this user" })
         }
